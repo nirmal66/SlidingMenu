fa9ecb5fc4dd2a18f51211b523c444b2df3c302d
nirmal6060@gmail.com <nirmal6060@gmail.com>
2014-11-20 16:16:39 +0530

    added fragment function

---
+++ src/com/example/allslidingmenu/NotificationFragment.java
@@ -0,0 +1,24 @@
+package com.example.allslidingmenu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NotificationFragment extends Fragment {
+
+	
+	
+    @Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.notification_fragment, container, false);
+		
+		
+		return view;
+	}
+	
+}
+
